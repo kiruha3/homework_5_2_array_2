f090509c1716dbb5b9f81019f7642d602c1e3a6f
kirill <kiruha3@mail.ru>
2023-06-26 00:27:42 +0700

    SECOND task

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
     }
+
     public static int[] generateRandomArray() {
         java.util.Random random = new java.util.Random();
         int[] arr = new int[30];
@@ -10,13 +12,31 @@ public class Main {
         }
         return arr;
     }
+
     public static void task1() {
         int[] arr = generateRandomArray();
         int summPerMonth = 0;
         for (int i = 0; i < arr.length; i++) {
-            summPerMonth+=arr[i];
-            System.out.printf("Сумма трат за месяц составила %d рублей",summPerMonth);
+            summPerMonth += arr[i];
         }
+        System.out.printf("Сумма трат за месяц составила %d рублей %n", summPerMonth);
+    }
 
+    public static void task2() {
+        int[] arr = generateRandomArray();
+        int minEl = 999_999_999;
+        int maxEl = 0;
+        int summPerMonth = 0;
+        for (int i = 0; i < arr.length; i++) {
+            if (minEl > arr[i]) {
+                minEl = arr[i];
+            }
+            if (maxEl < arr[i]) {
+                maxEl = arr[i];
+            }
+            // трассировка
+            // System.out.printf("%d - %d - %d %n",minEl,maxEl,arr[i]);
+        }
+        System.out.printf("Минимальная сумма трат за день составила %d рублей. %nМаксимальная сумма трат за день составил %d рублей", minEl, maxEl);
     }
 }
